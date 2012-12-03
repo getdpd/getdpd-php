7f1f33f9f9a1e65545d6d7fba9c90b822cafe1bf
Jacob Coby <jcoby@portallabs.com>
2012-12-03 17:11:54 -0500

    Initial release.

---
+++ README.md
@@ -0,0 +1,55 @@
+# GetDPD API
+
+The DPD API allows you to easily integrate your website with DPD.
+
+## What's Included
+
+The DPD cart API currently lets you list your storefronts (websites) and products.
+
+## Requirements
+
+PHP 5.2 or better with the cURL extension.
+
+## Usage
+
+    <?php
+    require_once "DPDCartApi.class.php";
+    
+    $dpd = new GetDPDApi("your username", "your key");
+    try {
+      $storefronts = $dpd->getStorefronts();
+      $products = $dpd->listProducts($storefronts[0]["id"]);
+    } catch(GetDPDApiError $e) {
+      die("Error! ".$e);
+    }
+    
+    var_export($products);
+    ?>
+
+See the [full reference](http://getdpd.com/docs/api/index.html) ([pdf version](http://getdpd.com/docs/api/DPDAPIReference.pdf))
+for a full reference.
+
+### Handling Errors
+
+A 404 Not Found error will throw a `GetDPDResourceNotFoundError`. A 401 Unauthorized error will throw a `GetDPDAuthenticationError`.
+If there is a cURL error or a non-200 HTTP response, the library will throw a `GetDPDApiError` with the cURL error message
+or the HTTP status code.
+
+### Verifying A Subscriber
+
+You can verify a subscriber's subscription status with the `verifySubscriber` methods. Pass in either a `username` or `id`.
+
+    <?php
+    $storefront_id = 1234;
+    $dpd->verifySubscriberById($storefront_id, array("id" => 123));
+    ?>
+
+or
+
+    <?php
+    $storefront_id = 1234;
+    $dpd->verifySubscriberByUsername($storefront_id, array('username' => 'example@example.com'));
+    ?>
+
+Both calls return the subscriber's status. Any subscriber with the `TRIAL`, `ACTIVE` or `CANCELED` status has access to the
+subscriber area in your DPD account.
---
+++ lib/GetDPDApi.php
@@ -0,0 +1,136 @@
+<?php
+
+class GetDPDApiError extends Exception { }
+class GetDPDResourceNotFoundError extends GetDPDApiError { }
+class GetDPDAuthenticationError extends GetDPDApiError { }
+
+/*
+ * This file is part of the DPD API package.
+ * (c) 2010-2012 Portal Labs, LLC <contact@portallabs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+class GetDPDApi
+{
+  const VERSION = '1.0';
+  
+  public $user = null;
+  public $key = null;
+  public $base_url = "api.getdpd.com/v2";
+  public $protocol = "https";
+  public $success = true;
+  
+  public function __construct($user, $key)
+  {
+    $this->user = $user;
+    $this->key = $key;
+  }
+  
+  public function ping()
+  {
+    return $this->doApiRequest('/', array(), 'GET');
+  }
+  
+  public function listStorefronts()
+  {
+    return $this->doApiRequest('/storefronts', array(), 'GET');
+  }
+  
+  public function getStorefront($id)
+  {
+    return $this->doApiRequest("/storefronts/{$id}", array(), "GET");
+  }
+  
+  public function listProducts($storefront_id=null)
+  {
+    return $this->doApiRequest('/products', array('storefront_id' => $storefront_id), 'GET');
+  }
+  
+  public function getProduct($id)
+  {
+    return $this->doApiRequest("/products/{$id}", array("id" => $id));
+  }
+  
+  public function listPurchases($params=array())
+  {
+    return $this->doApiRequest("/purchases", $params, 'GET');
+  }
+
+  public function getPurchase($website_id, $id)
+  {
+    return $this->doApiRequest("/purchases/{$id}", array("id" => $id));
+  }
+
+  public function listSubscribers($storefront_id, $params=array())
+  {
+    return $this->doApiRequest("/storefronts/{$storefront_id}/subscribers", $params, 'GET');
+  }
+  
+  public function getSubscriber($storefront_id, $id)
+  {
+    return $this->doApiRequest("/storefronts/{$storefront_id}/subscribers/{$id}", $params, 'GET');
+  }
+  
+  public function verifySubscriber($storefront_id, $params)
+  {
+    return $this->doApiRequest("/storefronts/{$storefront_id}/subscribers/verify", $params, 'GET');
+  }
+
+  public function doApiRequest($action, $params, $method='GET')
+  {
+    $this->success = false;
+    
+    $url = "{$this->protocol}://{$this->base_url}/{$action}";
+    
+    if($method == 'GET' && count($params) > 0)
+      $url.= "?".http_build_query($params);
+    
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_REFERER, "");
+    curl_setopt($ch, CURLOPT_USERPWD, "{$this->user}:{$this->key}");
+    //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    
+    switch(strtoupper($method)) 
+    {
+      case 'POST':
+        curl_setopt ($ch, CURLOPT_POST, 1);
+        curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+      break;
+      case 'DELETE':
+        curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+      break;
+      case 'PUT':
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        //curl_setopt ($ch, CURLOPT_HTTPHEADER, array ("Content-Type: application/x-www-form-urlencoded\n"));
+      break;
+    }
+    
+    $raw = curl_exec($ch);
+    if($raw === false)
+      throw new GetDPDApiError(curl_error($ch), null);
+      
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+    
+    curl_close($ch);
+
+    if($http_code == '404')
+      throw new GetDPDResourceNotFoundError($action);
+    else if($http_code == '401')
+      throw new GetDPDAuthenticationError("Unable to verify your getdpd.com API credentials.");
+    else if($http_code != '200')
+      throw new GetDPDApiError("Received error response: {$http_code}");
+    
+    if(strpos($content_type, 'application/json') !== false)
+      $response = json_decode($raw, true);
+    else
+      $response = $raw;
+    
+    $this->success = true;
+    return $response;
+  }
+}
